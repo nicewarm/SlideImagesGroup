627a8f0f27de8edba7b444b6b83f633a6117b90d
liukuo <kuoliu@meilishuo.com>
2015-12-29 17:16:16 +0800

    fix 优化滑动效果

--- app/src/main/java/com/example/mls/slideimagesgroup/DragLayout.java
+++ app/src/main/java/com/example/mls/slideimagesgroup/DragLayout.java
@@ -3,7 +3,6 @@ package com.example.mls.slideimagesgroup;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Debug;
-import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
@@ -26,11 +25,25 @@ public class DragLayout extends ViewGroup {
     /* 拖拽工具类 */
     private final ViewDragHelper mDragHelper;
 
-    /*子View列表*/
-    private List<View> views = new ArrayList<>();
-
     /*最右边的View*/
     private View mRightView;
+
+    private int kAvatarWidth;
+
+    public static final int kAvatarWidthDp = 40;
+    public static final int spaceDpValue = 26;
+    private int mVerDragRange;
+    private int mHorDranRange;
+    private int maxSpace;
+    private int minSpace;
+    private int space;
+
+    private OnSlideToLeftAndRightListener listener;
+
+
+    public interface OnSlideToLeftAndRightListener {
+        public void onSlide(double offset);
+    }
 
     private Context context;
 
@@ -45,14 +58,19 @@ public class DragLayout extends ViewGroup {
 
     public DragLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.context = context;
+        kAvatarWidth = dip2px(context, kAvatarWidthDp);
+        maxSpace = dip2px(context, spaceDpValue);
+        minSpace = -maxSpace;
+        space = minSpace;
         mDragHelper = ViewDragHelper.create(this, 1f, new DragHelperCallback());
         this.context = context;
 
     }
 
-    @Override
-    protected void onFinishInflate() {
-        super.onFinishInflate();
+    public static int dip2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
     }
 
     @Override
@@ -61,19 +79,18 @@ public class DragLayout extends ViewGroup {
 
         int maxWidth = MeasureSpec.getSize(widthMeasureSpec);
         int maxHeight = MeasureSpec.getSize(heightMeasureSpec);
-        setMeasuredDimension(
-                resolveSizeAndState(maxWidth, widthMeasureSpec, 0),
-                resolveSizeAndState(maxHeight, heightMeasureSpec, 0));
+        setMeasuredDimension(maxWidth, kAvatarWidth);
     }
-
-    private int mVerDragRange;
-    private int mHorDranRange;
-    private int maxSpace = 80;
-    private int minSpace = -80;
-    private int space = minSpace;
 
     public void setData(List<String> images) {
         removeAllViews();
+        initChildViews(images);
+        requestLayout();
+        invalidate();
+    }
+
+    /*初始化子view*/
+    private void initChildViews(List<String> images) {
         for (int i = 0; i < 4; i++) {
             ImageView imageView = new ImageView(context);
             imageView.setBackgroundColor(randomColor());
@@ -85,6 +102,7 @@ public class DragLayout extends ViewGroup {
                 public void onClick(View v) {
                     if (mDragOffset == -1) {
                         smoothSlideTo(1);
+                        mDragOffsetLast = 1;
                     }
                     if (mDragOffset == 1) {
                         Toast.makeText(context, finalI + "", Toast.LENGTH_SHORT).show();
@@ -93,19 +111,12 @@ public class DragLayout extends ViewGroup {
             });
             addView(imageView);
         }
-        requestLayout();
-        invalidate();
         if (getChildCount() > 0) {
-            for (int i = 0; i < getChildCount(); i++) {
-                View view = getChildAt(i);
-                views.add(view);
-                if (i == getChildCount() - 1) {
-                    mRightView = getChildAt(i);
-                }
-            }
+            mRightView = getChildAt(getChildCount() - 1);
         }
     }
 
+    /*随机颜色*/
     private int randomColor() {
         int colorValue = (int) (Math.random() * (16777216 - 1) + 1) * -1;
         String hex = Integer.toHexString(colorValue);
@@ -113,6 +124,7 @@ public class DragLayout extends ViewGroup {
     }
 
 
+    /*初始化布局子view*/
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         int wid = 0;
@@ -135,7 +147,7 @@ public class DragLayout extends ViewGroup {
         boolean shouldIntercept = mDragHelper.shouldInterceptTouchEvent(ev);
         int action = ev.getActionMasked();
         if (action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_MOVE) {
-            // action_down时就让mDragHelper开始工作，否则有时候导致异常 他大爷的
+            // 只有move事件，让dragHelper自己处理，其余事件不拦截，否则onClick就失效了。
             if (action == MotionEvent.ACTION_MOVE) {
                 mDragHelper.processTouchEvent(ev);
             }
@@ -148,14 +160,13 @@ public class DragLayout extends ViewGroup {
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        // 统一交给mDragHelper处理，由DragHelperCallback实现拖动效果
+        // 只要有事件拦截，就全部交给mDragHelper处理，由DragHelperCallback实现拖动效果
         mDragHelper.processTouchEvent(ev);
-        if (ev.getAction() == MotionEvent.ACTION_UP) {
-            return false;
-        }
         return true;
     }
 
+
+    // 判断当前点击时间是否在View中
     private boolean isViewHit(View view, int x, int y) {
         int[] viewLocation = new int[2];
         view.getLocationOnScreen(viewLocation);
@@ -167,12 +178,14 @@ public class DragLayout extends ViewGroup {
                 screenY >= viewLocation[1] && screenY < viewLocation[1] + view.getHeight();
     }
 
+    //    探测子View的右边界
     private int getCurrentViewRight(View view) {
         int index = indexOfChild(view);
         int right = view.getWidth() * index + index * maxSpace;
         return right;
     }
 
+    //    探测子View的左边界
     private int getCurrentViewLeft(View view) {
         int index = indexOfChild(view);
         int left = view.getWidth() * index + index * minSpace;
@@ -180,6 +193,7 @@ public class DragLayout extends ViewGroup {
     }
 
 
+    //    让动画持续
     @Override
     public void computeScroll() {
         if (mDragHelper.continueSettling(true)) {
@@ -187,6 +201,7 @@ public class DragLayout extends ViewGroup {
         }
     }
 
+    //    指定滑动到某个位置
     private boolean smoothSlideTo(float slideOffset) {
         int right = slideOffset == 0f ? getCurrentViewLeft(mRightView) : getCurrentViewRight(mRightView);
         if (mDragHelper.smoothSlideViewTo(mRightView, right, 0)) {
@@ -197,6 +212,7 @@ public class DragLayout extends ViewGroup {
     }
 
     private double mDragOffset = -1;
+    private double mDragOffsetLast = -1;
 
     /**
      * 这是拖拽效果的主要逻辑
@@ -205,6 +221,7 @@ public class DragLayout extends ViewGroup {
 
         @Override
         public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
+            //    按比例计算滑动的位移，在maxSpace和minSpace之间
             int minLeft = getCurrentViewLeft(changedView);
             int maxRight = getCurrentViewRight(changedView);
             int step = maxRight - minLeft;
@@ -219,12 +236,16 @@ public class DragLayout extends ViewGroup {
             if (space < minSpace) {
                 space = minSpace;
             }
+
+            if (listener != null) {
+                listener.onSlide(mDragOffset);
+            }
+
             requestLayout();
         }
 
         @Override
         public boolean tryCaptureView(View child, int pointerId) {
-            // 两个子View都需要跟踪，返回true
             return true;
         }
 
@@ -240,17 +261,22 @@ public class DragLayout extends ViewGroup {
 
         @Override
         public void onViewReleased(View releasedChild, float xvel, float yvel) {
-            int right;
-            if (mDragOffset > 0) {
+            //   在释放滑动事件后，继续移动view，达到惯性的效果
+            int right = 0;
+            if (mDragOffset - mDragOffsetLast > 0) {
                 right = getCurrentViewRight(releasedChild);
+                mDragHelper.settleCapturedViewAt(right, 0);
+                invalidate();
+                mDragOffsetLast = 1;
             } else {
                 right = getCurrentViewLeft(releasedChild);
+                mDragHelper.settleCapturedViewAt(right, 0);
+                invalidate();
+                mDragOffsetLast = -1;
             }
-            mDragHelper.settleCapturedViewAt(right, 0);
-            invalidate();
         }
 
-
+        //  判断滑动的边界，不能出借
         @Override
         public int clampViewPositionHorizontal(View child, int left, int dx) {
             final int newLeft = Math.min(Math.max(left, getCurrentViewLeft(child)), getCurrentViewRight(child));
